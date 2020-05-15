1df1ffcf273a235eea81168a7ec1e05311712867
Wirgiliusz <kamx98@gmail.com>
2020-05-15 19:43:45 +0200

    Defnicja pol labiryntu

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define WS 179
+#define WAS 180
+#define AS 191
+#define WD 192
+#define AWD 193
+#define ASD 194
+#define WDS 195
+#define AD 196
+#define WASD 197
+#define AW 217
+#define DS 218
+
+
+int main() {
+    printf("%c", 197);
+
+    return 0;
+}
